ecd9d11a04a0429fe93223d828b140efd6b2b807
Yusufcan Ulusoy <yusufcan.ulusoy@agu.edu.tr>
2023-12-25 17:42:41 +0300

    Model Optimization

--- app.py
+++ app.py
@@ -5,35 +5,34 @@ from transformers import pipeline
 
 app = Flask(__name__)
 
-# Initialize the model (example with a sentiment analysis model)
-model_name = "savasy/bert-base-turkish-sentiment-cased"  # Replace with your chosen model
-nlp = None
-
 class NewsItem(BaseModel):
     content: str
 
-@app.before_first_request
 def load_model():
-    global nlp
-    nlp = pipeline("sentiment-analysis", model=model_name, tokenizer=model_name)
+    # Dynamically load the model
+    model_name = "savasy/bert-base-turkish-sentiment-cased"
+    model = pipeline("sentiment-analysis", model=model_name, tokenizer=model_name)
+    return model
 
 @app.route('/analyze-news/', methods=['POST'])
 def analyze_news():
     try:
-        # Parse and validate input
         data = request.get_json()
         item = NewsItem(**data)
-
-        # Analyze news content
+        
+        # Load model for each request (consider caching if performance is an issue)
+        nlp = load_model()
         result = nlp(item.content)
+
+        # Optional: Clear model from memory if needed (can impact performance)
+        # del nlp
+
         return jsonify(result)
 
     except ValidationError as e:
-        # Handle pydantic validation error
         return jsonify({'error': str(e)}), 400
 
     except Exception as e:
-        # Handle general errors
         raise InternalServerError(description=str(e))
 
 if __name__ == '__main__':
